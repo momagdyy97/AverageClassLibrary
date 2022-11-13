74ecb145b9cb8d5c7bf09f9b08f3f31e830d420c
Mohamed Magdy <momagdyy97@gmail.com>
2022-11-13 12:38:53 +0200

    Adding AverageClassLibrary files.

---
+++ CalculatorConsoleApp/Calculate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    public class Calculate
+    {
+        public int Average(int[] nums)
+        {
+            int total = 0;
+            foreach (var num in nums)
+            {
+                total += num;
+            }
+            return total / nums.Count();
+        }
+
+    }
+}
---
+++ CalculatorConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using CalculatorConsoleApp;
+using System;
+
+namespace BasicCalculatorConsoleApp
+{
+    public class Program {
+
+        public static void Main(string[] args) {
+            int [] array = {1, 2, 3};
+            Calculate calc = new Calculate ();
+            var result = calc.Average(array);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        
+        }
+
+    
+    }
+}
